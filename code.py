5c2317b0fb2f29db01b2d7fbd2f82ca35850dd54
barrondd <102961777+barrondd@users.noreply.github.com>
2022-06-06 13:31:33 +0200

    Python

---
+++ code.py
@@ -0,0 +1,20 @@
+print("Podaj a: ")
+a=float(input())
+print("Podaj b: ")
+b=float(input())
+def sumaa(a,b):
+    suma=a + b
+    print("suma = ", suma)
+def roznicaa(a,b):
+    roznica=a - b
+    print("roznica = ", roznica)
+def iloczynn(a,b):
+    iloczyn=a * b
+    print("iloczyn = ", iloczyn)
+def ilorazz(a,b):
+    iloraz=a / b
+    print("iloraz= ", iloraz)
+sumaa(a,b)
+roznicaa(a,b)
+iloczynn(a,b)
+ilorazz(a,b)
